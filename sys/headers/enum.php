a880c219522eb7dc03d1ab64d016df2b7581313f
tohya-ryu <ryu@tohya.net>
2023-07-04 07:50:51 +0200

    add method enum

--- sys/headers/enum.php
+++ sys/headers/enum.php
@@ -5,3 +5,12 @@ Enum Env: int
     case Development = 0;
     case Production  = 1;
 }
+
+Enum Method: string
+    case GET    = 'get';
+    case DELETE = 'delete';
+    case PATCH  = 'patch'
+    case POST   = 'post';
+    case PUT    = 'put';
+}
+
